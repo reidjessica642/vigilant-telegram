9c6d53a6563a6bdf0d986eb5f559463fd7f2a298
reidjessica642 <reidjessica642@gmail.com>
2026-04-16 18:49:29 -0500

    Added array to index.js - updated README

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log('Exercise');
+
+// create a random array
+const randomArray =
+[
+    2,
+    3,
+    555,
+    324,
+    11,
+    4352,
+    1244
+]
+
+// call array - don't care to iterate
+console.log(randomArray);
